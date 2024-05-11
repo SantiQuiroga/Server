9851505a05338ffb53262f434741245dacced974
SantiQuiroga <quiroga.default.santiago@gmail.com>
2024-05-11 13:11:24 -0400

    add signal handling and python script execution

--- main.c
+++ main.c
@@ -1,11 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 #define PORT 8080
+#define BUFFER_SIZE 1024
 #define MAX_CONNECTIONS 30
+
+char* execute_python_script(char* script_path) {
+    FILE *python_output;
+    char python_command[BUFFER_SIZE];
+    static char python_buffer[BUFFER_SIZE * BUFFER_SIZE];
+    int status;
+    pid_t pid;
+
+    snprintf(python_command, sizeof(python_command), "python3 %s", script_path);
+    python_output = popen(python_command, "r");
+    if (python_output == NULL) {
+        perror("failed to run python script");
+        return NULL;
+    }
+
+    char line[BUFFER_SIZE];
+    python_buffer[0] = '\0';
+    while (fgets(line, sizeof(line), python_output) != NULL) {
+        strncat(python_buffer, line, sizeof(python_buffer) - strlen(python_buffer) - 1);
+    }
+
+    pid = waitpid(-1, &status, WNOHANG);
+    if (pid == 0) {
+        if (pclose(python_output) == -1) {
+            perror("failed to close python script process");
+        }
+    }
+
+    return python_buffer;
+}
+
+void sigchld_handler() {
+    while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+void setup_sigchld_handler() {
+    struct sigaction sa;
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+}
 
 int create_server_socket() {
     int server_fd;
